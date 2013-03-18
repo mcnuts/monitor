49c92b3704c78f089b7f955b0e232e48b6b61e9c
mcnuts <laurie@infitialis.com>
2013-03-18 16:16:43 +0000

    Update ping.pl

--- tests/ping.pl
+++ tests/ping.pl
@@ -1,20 +1,37 @@
 #!/usr/bin/perl
 
 use Net::Ping;
+use DBI;
 
-$my_addr = "10.14.14.1";
-@hostname = qw# jolene bella ruby susie #;
-foreach $client ('10.14.14.18', '10.14.14.14', '10.14.14.22', '10.8.12.14')
-	{
-		$p = Net::Ping->new("icmp");
-		$p->bind($my_addr);
-    		{
-			$a = shift(@hostname);
-        		print "$a is ";
-        		print "NOT " unless $p->ping($client, 1);
-        		print "reachable.\n";
-        		sleep(1);
-		}
-		$p->close();
-	}
+$username = 'root';$password = '#';$database = 'monitor';$hostname = 'localhost';
+$dbh = DBI->connect("dbi:mysql:database=$database;" .
+ "host=$hostname;port=3306", $username, $password);
 
+$SQL= "select ip, name from servers";
+
+$Select = $dbh->prepare($SQL);
+$Select->execute();
+
+$charlie = $dbh->selectrow_array("select ip from servers where name='charlie'");
+
+while($Row=$Select->fetchrow_hashref)
+{
+  @client = "$Row->{ip}";
+
+        $my_addr = "$charlie";
+        @hostname = $Row->{name};
+        foreach (@client)
+                {
+                        $p = Net::Ping->new("icmp");
+                        $p->bind($my_addr);
+                        {
+                                $a = shift(@hostname);
+                                print "$a is ";
+                                print "NOT " unless $p->ping($client, 1);
+                                print "reachable.\n";
+                                sleep(1);
+                        }
+                        $p->close();
+                }
+
+}
